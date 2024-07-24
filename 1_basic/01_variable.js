80531ea113a12c85096706947d993e7b26188859
Sekh Ashik Billa <125138263+sekhashik09@users.noreply.github.com>
2024-07-24 13:24:43 +0000

    console config done

---
+++ 1_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=1332732
+let accountEmail="ashiksekh8967@gmail.com"
+var accountPassword="685455"
+accountCity="kolkata"
+let accountState;
+
+// accountId=2      (const) is for fix values we can't change 
+
+
+//alawys use let and const
+
+accountEmail="aa@gmail.com"
+accountPassword="44652565"
+accountCity="delhi"
+
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
---
+++ 1_basic/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let name="ashik"    //String
+let age=21         //Number
+let isLoggedIn=true  //Boolean
+
+/*
+
+number = 2^53
+bigint
+string = ""
+boolean = true/false
+null = standalone value (represent an empty value)
+undefined = not define
+symbol = unique
+
+*/
+console.log(typeof null);
